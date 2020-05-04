ab9c5b56c081093cc93b1ceb9525906e392f86e7
Brus Palaj <brusp@bruss-mbp.home>
2020-05-04 19:04:03 -0400

    finally got it to append burgers to page, now working on some styling format etc to make it look more appealing

--- burger/controllers/burgersControllers.js
+++ burger/controllers/burgersControllers.js
@@ -8,16 +8,16 @@ var burger = require("../models/burger.js");
 // Create all our routes and set up logic within those routes where required.
 router.get("/", function(req, res) {
   burger.selectAll(function(data) {
-    var hbsObject = {
-      burgers: data
-    };
-    console.log(hbsObject);
-    res.render("index", hbsObject);
+    // var hbsObject = {
+    //   burgers: data
+    // };
+    // console.log(hbsObject);
+    res.render("index", {burgers: data});
   });
 });
 
 router.post("/api/burgers", function(req, res) {
-  burger.insertOne(["burger_name", "devoured"], [req.body.name, req.body.devoured], function(result) {
+  burger.insertOne(["burger_name", "devoured"], [req.body.burger_name, req.body.devoured], function(result) {
     // Send back the ID of the new quote
     res.json({ id: result.insertId });
   });
